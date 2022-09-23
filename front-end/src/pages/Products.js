058b0f3440794b1dbf5ef0ab3efb4e5879036a97
thais oliveira <atlasoliveira2001@gmail.com>
2022-09-23 17:09:10 -0400

    feat: add pages

---
+++ front-end/src/pages/Products.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import Navbar from '../components/Navbar';
+import ProductCard from '../components/ProductCard';
+import instance from '../services/axiosInstance';
+
+function Products() {
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    async function getAllProducts() {
+      try {
+        const response = await instance.get('customer/products');
+        setProducts(response);
+      } catch (error) {
+        return error.response.data;
+      }
+    }
+
+    getAllProducts();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        {
+          products.map((product) => (
+            <ProductCard
+              key={ product.id }
+              product={ product }
+            />))
+        }
+      </div>
+    </>
+  );
+}
+
+export default Products;
