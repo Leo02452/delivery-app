4d40ab3ff8d11f52d2fc7d6a8823113ca1e72810
Leo Araujo <leonardo_02452@hotmail.com>
2022-09-23 19:19:09 -0300

    feat: customer product route

--- front-end/src/components/AppRoutes.js
+++ front-end/src/components/AppRoutes.js
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import Products from '../pages/Products';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/customer/products" element={ <Products /> } />
       <Route exact path="/" element={ <Navigate to="/login" /> } />
     </Routes>
   );
