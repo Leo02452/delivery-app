39490aafceee85d363b21e8f506cf1a77443fa90
Tomas Breuckmann <tomas.yoga@yahoo.com>
2022-09-28 14:06:25 -0300

    fix: button cart redirect bug when redirect

--- front-end/src/components/styles/buttonCart.styles.js
+++ front-end/src/components/styles/buttonCart.styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { BasicMain, BasicButton } from '../../styles/generals.styles';
+import { BasicMain } from '../../styles/generals.styles';
 
 export const ButtonMain = styled(BasicMain)`
   background-color: ${(props) => props.theme.productCard.background};
@@ -10,15 +10,16 @@ export const ButtonMain = styled(BasicMain)`
   bottom: 12px;
   `;
 
-export const Button = styled(BasicButton)`
+export const Button = styled.button`
   background: ${(props) => props.theme.buttonCard.background};
   color: ${(props) => props.theme.buttonCard.color};
   border: 1px solid;
   border-color: ${(props) => props.theme.buttonCard.borderColor};
-  /* font-size: 20px; */
   margin: 0;
   width: 150px;
   padding: 12px 20px;
   text-align: right;
   cursor: pointer;
+  border-radius: 4px;
+  font-weight: 600;
 `;
