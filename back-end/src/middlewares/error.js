8f383da10c4ff1cf2c88358db9bf847e10b82c9b
Leo Araujo <leonardo_02452@hotmail.com>
2022-09-28 14:52:52 -0300

    feat: add unauthorized error in errorHandle

--- back-end/src/middlewares/error.js
+++ back-end/src/middlewares/error.js
@@ -1,9 +1,11 @@
 const errorMiddleware = (err, _req, res, _next) => {
     switch (err.name) {
-      case 'NotFoundError':
-        return res.status(404).json({ message: err.message });
       case 'ZodError':
         return res.status(400).json({ message: err.message });
+      case 'UnauthorizedError':
+        return res.status(401).json({ message: err.message });
+      case 'NotFoundError':
+        return res.status(404).json({ message: err.message });
       case 'ConflictError':
         return res.status(409).json({ message: err.message });
       default:
