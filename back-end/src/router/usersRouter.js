0c6398eb725b195d711aa697de54db39bf3afe2f
Leo Araujo <leonardo_02452@hotmail.com>
2022-10-14 16:26:44 -0300

    chore: useless endpoint test

--- back-end/src/router/usersRouter.js
+++ back-end/src/router/usersRouter.js
@@ -7,7 +7,7 @@ const { userValidation } = require('../middlewares/userValidation');
 const route = Router();
 
 route.get('/search', usersController.list);
-route.get('/:id', usersController.findByPk);
+// route.get('/:id', usersController.findByPk);
 route.delete('/:id', tokenValidation, roleValidation, usersController.delete);
 route.post('/', userValidation, tokenValidation, roleValidation, usersController.create);
 
