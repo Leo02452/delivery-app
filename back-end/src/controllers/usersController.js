7e41637b27be8f14572a507ede35bebf56801575
Leo Araujo <leonardo_02452@hotmail.com>
2022-10-14 17:22:11 -0300

    perf: remove useless endpoint

--- back-end/src/controllers/usersController.js
+++ back-end/src/controllers/usersController.js
@@ -8,13 +8,6 @@ const usersController = {
     res.status(200).json(users);
   },
 
-  async findByPk(req, res) {
-    const { id } = req.params;
-    const user = await usersService.findByPk(id);
-
-    res.status(200).json(user);
-  },
-
   async create(req, res) {
     const createdUser = await usersService.create(req.body);
 
