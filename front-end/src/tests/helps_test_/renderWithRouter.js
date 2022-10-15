ad98527107e4787fbb2b38e8a2fd53ed8b82d3bd
Leo Araujo <leonardo_02452@hotmail.com>
2022-10-15 17:43:39 -0300

    test: add render with router, redux e sc providers

--- front-end/src/tests/helps_test_/renderWithRouter.js
+++ front-end/src/tests/helps_test_/renderWithRouter.js
@@ -1,17 +1,21 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import store from '../../redux/store';
 import Global from '../../styles/global.styles';
 import light from '../../styles/lightTheme';
 
 const renderWithrouter = (component) => ({
   ...render(
     <BrowserRouter>
-      <ThemeProvider theme={ light }>
-        <Global />
-        { component }
-      </ThemeProvider>
+      <Provider store={ store }>
+        <ThemeProvider theme={ light }>
+          <Global />
+          {component}
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>,
   ),
 });
