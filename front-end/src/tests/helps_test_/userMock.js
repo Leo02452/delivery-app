37896f773cd23b3fb72d84dd4d74b9a4d723179b
Leo Araujo <leonardo_02452@hotmail.com>
2022-10-21 16:50:03 -0300

    test: add get users response mock

---
+++ front-end/src/tests/helps_test_/userMock.js
@@ -0,0 +1,34 @@
+const usersMock = [
+  {
+    id: 1,
+    email: 'adm@deliveryapp.com',
+    name: 'Delivery App Admin',
+    role: 'administrator',
+  },
+  {
+    id: 2,
+    email: 'fulana@deliveryapp.com',
+    name: 'Fulana Pereira',
+    role: 'seller',
+  },
+  {
+    id: 3,
+    email: 'zebirita@email.com',
+    name: 'Cliente Zé Birita',
+    role: 'customer',
+  },
+  {
+    id: 4,
+    email: 'analauraamoreira_nogueira62@gmail.com',
+    name: 'Ana Laura a Moreira',
+    role: 'seller',
+  },
+  {
+    id: 5,
+    email: 'hugobcosta.martins80@live.com',
+    name: 'Hugo b Costa',
+    role: 'customer',
+  },
+];
+
+export default usersMock;
